b27e400a14f0f9a390e7dfeb16ff113f0d510b00
logicalforhad <forhad.univdhaka@gmail.com>
2015-02-18 23:33:37 +0600

    Angular Js added with Read functionality

---
+++ ODataCrud/ODataCrud/Scripts/app/productController.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('app', ['ngRoute']);
+app.controller('productController', function ($scope, $http) {
+    $scope.message = "how are you?";
+    $http.get('/odata/Products').success(function (data) {
+        $scope.productList = data.value;
+    })
+            .error(function (error) {
+                console.log(error);
+            });
+});
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/about', {
+            templateUrl: 'Scripts/templates/about.html'
+        })
+        .when('/contact', {
+            templateUrl: 'Scripts/templates/contact.html'
+        })
+        .when('/', {
+            templateUrl: 'Scripts/templates/home.html'
+        });
+});
